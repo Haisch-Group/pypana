43b973bf20a6f07e39bb96a087c9eb25dc040e68
Kevin Maier <kevin.r.maier@tum.de>
2023-02-13 09:21:07 +0100

    Code for saving workspace via pickle module

---
+++ Save_values.py
@@ -0,0 +1,29 @@
+import pickle
+
+
+def is_picklable(obj):
+    try:
+        pickle.dumps(obj)
+    except Exception:
+        return False
+    return True
+
+
+if __name__ == "__main__":
+    path = input("please enter the desired path")
+    bk = {}
+    for k in dir():
+        obj = globals()[k]
+        if is_picklable(obj):
+            try:
+                bk.update({k: obj})
+            except TypeError:
+                pass
+    # to save session
+    with open(f'{path}', 'wb') as f:
+        pickle.dump(bk, f)
+
+    # to load your session
+
+    #with open('{path}', 'rb') as f:
+        #bk_restore = pickle.load(f)
