ef0bd825fd6dfead6e74353f5169af60fe06ce32
Kevin Maier <kevin.r.maier@tum.de>
2024-04-30 09:51:47 +0200

    Changed plot_singledata back from dictionary logic Is now called via plot_cutdata and plot_fulldata, which give it the required data extracted from the dictionary

--- Conc.py
+++ Conc.py
@@ -31,6 +31,14 @@ def select_data(Cn, msmt_nrs):
     return sel_Cn
 
 
+def get_meanconc(data, conc_key):
+    """gives mean and std of a concentration array based on the key given as str
+    call: get_meanconc(data, "Cn") """
+    data["mean_"+conc_key] = np.nanmean(data[conc_key], 1)
+    data["std_"+conc_key] = np.nanstd(data["Cn"], 1)
+    return data
+
+
 def cut_time(data, start, end):
     """can be used to cut conc array time wise"""
     data["cut_Cn"] = data["Cn"][:, start:end]
@@ -38,15 +46,22 @@ def cut_time(data, start, end):
     return data
 
 
-def get_meanconc(data):
-    data["conc_n"] = np.nanmean(data["Cn"], 1)
-    data["std_n"] = np.nanstd(data["Cn"], 1)
-    return data
+def plot_fulldata(data, scan_nr):
+    Cn, el_time, mean_Cn, std_Cn, filename = (data["Cn"], data["el_time"], data["mean_Cn"], data["std_Cn"],
+                                              data["filename"])
+    ax = plot_singledata(Cn, el_time, mean_Cn, std_Cn, filename, scan_nr)
+    return ax
 
 
-def plot_singledata(data, scan_nr):  # does not work to plot cut cn anymore
-    """plots complete data"""
-    Cn, el_time, conc_n, std_n = data["Cn"], data["el_time"], data["conc_n"], data["std_n"]
+def plot_cutdata(data, scan_nr):
+    Cn, el_time, mean_Cn, std_Cn, filename = (data["cut_Cn"], data["cut_time"], data["mean_cut_Cn"],
+                                            data["mean_cut_std_Cn"], data["filename"])
+    ax = plot_singledata(Cn, el_time, mean_Cn, std_Cn, filename, scan_nr)
+    return ax
+
+
+def plot_singledata(Cn, el_time, mean_Cn, std_Cn, filename, scan_nr):  # does not work to plot cut cn anymore
+    """plots scan data"""
     plot_nr = py_logic_converter(scan_nr)
     cm = 1/2.54  # inches to cm
     fig, ax = plt.subplots(figsize=(18.5*cm, 10*cm))  # height with title 12, without 10
@@ -70,68 +85,26 @@ def plot_singledata(data, scan_nr):  # does not work to plot cut cn anymore
         legend_entries = []
         for k in plot_nr:
             legend_entries.append(input(f"Please enter the legend entry for measurement {k+1}"))
-    [print(f"measurement {k+1} conc. = " + "{:e}".format(float(conc_n[k])) + u"\u00B1" +
-            "{:e}".format(float(std_n[k])) + " P/cm" + u"\u00B3") for k in plot_nr]
+    [print(f"measurement {k+1} conc. = " + "{:e}".format(float(mean_Cn[k])) + u"\u00B1" +
+            "{:e}".format(float(std_Cn[k])) + " P/cm" + u"\u00B3") for k in plot_nr]
     #mpldatacursor.datacursor(ax)
     plt.legend(legend_entries)
 
     fileaddition = input("Please enter a fileaddition")
-    path = data["filename"][:-4] + "_" + fileaddition + ".png"
+    path = filename[:-4] + "_" + fileaddition + ".png"
     plt.savefig(path, transparent=True)
 
     plt.show()
     return ax
 
 
-# change plot single_data and plot_cutdata, so that they just call a bigger plot function and give it different input
-
-
-def plot_cutdata(data, scan_nr):  # does not work to plot cut cn anymore
-    """plots complete data"""
-    Cn, el_time, conc_n, std_n = data["cut_Cn"], data["cut_time"], data["cut_conc_n"], data["cut_std_n"]
-    plot_nr = py_logic_converter(scan_nr)
-    cm = 1/2.54  # inches to cm
-    fig, ax = plt.subplots(figsize=(18.5*cm, 10*cm))  # height with title 12, without 10
-    if len(plot_nr) == 1:
-        k = plot_nr[0]
-        ax.scatter(el_time, Cn[k, :], edgecolor='black')
-    else:
-        for k in plot_nr:
-            ax.scatter(el_time, Cn[k, :], edgecolor='black')
-    ax.xaxis.set_major_formatter(ticker.ScalarFormatter())
-    ax.set(xlabel='Elapsed Time / s',
-           ylabel='Particle Number Concentration / $\mathregular{1/cm^3}$')
-    # plt.title(input("Please enter the title of the figure"), wrap=True, y=1.08)
-    fig.subplots_adjust(top=0.95)  # 0.8 when title is active, when not 0.95 looks good also change figsize!
-    if len(plot_nr) == 1:
-        k = plot_nr[0]
-        legend_entries = [input(f"Please enter the legend entry for measurement {k+1}")]
-        # print(f"measurement {k} conc. = " + "{:e}".format(float(conc_n[k])) + u"\u00B1" +
-        #       "{:e}".format(float(std_n[k])) + " P/cm" + u"\u00B3")
-    else:
-        legend_entries = []
-        for k in plot_nr:
-            legend_entries.append(input(f"Please enter the legend entry for measurement {k+1}"))
-    # [print(f"measurement {k+1} conc. = " + "{:e}".format(float(conc_n[k])) + u"\u00B1" +
-    #        "{:e}".format(float(std_n[k])) + " P/cm" + u"\u00B3") for k in plot_nr]
-    # mpldatacursor.datacursor(ax)
-    plt.legend(legend_entries)
-
-    fileaddition = input("Please enter a fileaddition")
-    path = data["filename"][:-4] + "_" + fileaddition + ".png"
-    plt.savefig(path, transparent=True)
-
-    plt.show()
-    return ax
-
-
-def plot_timeline(conc_n, std_n, start_time, start, end):
+def plot_timeline(mean_Cn, std_Cn, start_time, start, end):
     """plots concentration timeline with mean conc of chosen single CPC scans
     only works with more than 1 datapoints"""
     cm = 1/2.54  # inches to cm
     fig, ax = plt.subplots(figsize=(18.5*cm, 10*cm))  # height with title 12, without 10
-    ax.scatter(start_time[start:end], conc_n[start:end], edgecolor='black')
-    ax.errorbar(start_time[start:end], conc_n[start:end], yerr=std_n[start:end], fmt="o")
+    ax.scatter(start_time[start:end], mean_Cn[start:end], edgecolor='black')
+    ax.errorbar(start_time[start:end], mean_Cn[start:end], yerr=std_Cn[start:end], fmt="o")
     ax.xaxis.set_major_formatter(ticker.ScalarFormatter())
     ax.xaxis.set_tick_params(reset=True)
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
@@ -140,8 +113,8 @@ def plot_timeline(conc_n, std_n, start_time, start, end):
     # plt.title(input("Please enter the title of the figure"), wrap=True, y=1.08)
     fig.subplots_adjust(top=0.95)  # 0.8 when title is active, when not 0.95 looks good also change figsize!
     legend_entries = [input(f"Please enter the legend entry for the measurement")]
-    [print(f"measurement {k} conc. = " + "{:e}".format(float(conc_n[k])) + u"\u00B1" +
-           "{:e}".format(float(std_n[k])) + " P/cm" + u"\u00B3") for k in np.arange(start, end)]
+    [print(f"measurement {k} conc. = " + "{:e}".format(float(mean_Cn[k])) + u"\u00B1" +
+           "{:e}".format(float(std_Cn[k])) + " P/cm" + u"\u00B3") for k in np.arange(start, end)]
     #mpldatacursor.datacursor(ax)
     plt.legend(legend_entries)
 
