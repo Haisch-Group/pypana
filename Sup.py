5650c0dc26016276308d438e5c3f841c3299e913
KevinRMaier <kevin.r.maier@tum.de>
2024-09-12 16:53:35 +0200

    added set_filename(), conversion standard to volumetric flow and other direction as well as conversion °C to K and other direction and mbar to kPa and other direction transfered lognormal function for testing purposes to here but will probably move it later again

--- Sup.py
+++ Sup.py
@@ -11,7 +11,9 @@ Created 2024-03-20 from get_filenames.py and other small scripts
 
 
 from tkinter import Tk
-from tkinter.filedialog import askopenfilename, askopenfilenames
+from tkinter.filedialog import askopenfilename, askopenfilenames, asksaveasfilename
+import numpy as np
+import math
 
 
 def get_filename():
@@ -34,6 +36,17 @@ def get_filenames():
     return filenames
 
 
+def set_filename():
+    """set filename via UI"""
+    popup = Tk()
+    popup.attributes('-topmost', 1)
+    popup.withdraw()
+    filename = asksaveasfilename(filetypes=(("All Files", "*"), ("png file", ".png"), ("csv file", "*.csv"),
+                                            ("dill file", ".dill")))
+    print(filename)
+    return filename
+
+
 def get_variable_name(some_variable):
     for name, value in globals().items():
         if value is some_variable:
@@ -54,10 +67,48 @@ def normal_logic_converter(nr_list):
     return normal_nr_list
 
 
-def convert_standard_to_volumetric_flow(standard_flow, T_flow, p_flow, T_standard, p_standard):
+def convert_standard_to_volumetric_flow(standard_flow, T_flow, p_flow, T_standard, p_standard, T_unit):
     """converts standard flow rate given by mass flow controllers to volumetric flow rate as required for calculation
     of aerosol concentrations based on ideal gas law
-    units must match, so T should be given in °C, p should be given in matching units, Pa, kPa, mbar, or bar
+    units must match, so T should be given in K, or °C, p should be given in matching units, Pa, kPa, mbar, or bar
     formula also given in TSI Application Note FLOW-004"""
-    volumetric_flow = standard_flow*((T_flow+273.15)/(T_standard+273.15)*(p_standard/p_flow))
+    if T_unit == "°C":
+        T_flow = convert_C_to_K(T_flow)
+    elif T_unit == "K":
+        pass
+    else:
+        print(f"{T_unit} is not a viable Temperature unit, use °C or K")
+    volumetric_flow = standard_flow * ((T_flow) / (T_standard) * (p_standard / p_flow))
     return volumetric_flow
+
+
+def convert_C_to_K(T_in_C):
+    """convert Temperature from °C to K"""
+    T_in_K = T_in_C + 273.15
+    return T_in_K
+
+
+def convert_K_to_C(T_in_K):
+    """convert Temperature from K to °C"""
+    T_in_C = T_in_K - 273.15
+    return T_in_C
+
+
+def convert_mbar_to_kPa(p_in_mbar):
+    """convert Pressure from mbar to kPa"""
+    p_in_kPa = p_in_mbar/10
+    return p_in_kPa
+
+
+def convert_kPa_to_mbar(p_in_kPa):
+    """convert Pressure from kPa to mbar"""
+    p_in_mbar = p_in_kPa/10
+    return p_in_mbar
+
+
+def lognormal_test(x_lower=5, x_upper=1000, x_steps=99, conc=1E5, dg=80, sigma_g=1.15):
+    """log-normal function with A being a scale factor, m being the median and sigma being the geometric
+        standard deviation"""
+    X = np.logspace(x_lower, x_upper, x_steps, endpoint=True, base=10.0)
+    C = (np.exp(-((np.log(X/dg))**2)/(2*np.log(sigma_g)**2))/(np.log(sigma_g)*X*np.sqrt(2*math.pi)))
+    return X, C
