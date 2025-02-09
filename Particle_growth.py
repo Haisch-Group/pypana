c9c3c7a058f15c465dff7a15508701947953cb86
KevinRMaier <kevin.r.maier@tum.de>
2025-02-09 17:11:27 +0100

    created files for calculations

---
+++ Particle_growth.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Particle_growth.py
+
+Script for calculating particle growth and related parameters.
+
+Created 2025-02-09
+@written by Kevin Maier (kevin.r.maier@tum.de)
+"""
+
