84f03764430a5ab90dcc53a28bbcc27aafb8493e
KevinRMaier <kevin.r.maier@tum.de>
2025-11-02 11:42:46 +0100

    adjustet calls for X and C from data to work with different C values, not only mean_C

--- histogram_fit.py
+++ histogram_fit.py
@@ -3,11 +3,15 @@ import numpy as np  # KM already in Dist
 import pandas as pd  # KM already in Dist
 import math  # KM already in Dist but as math only -> rewrite to import math -> done, was only one use
 from scipy import optimize # KM already in Dist sd optimize -> rewrite -> done, was only one use
+import Sup
 
 def lognormal_function(x, mu, sigma, A):
+    """just definition of the lognormal function that should be fitted"""
     return A*(np.exp(-((np.log(x/mu))**2)/(2*np.log(sigma)**2))/(np.log(sigma)*x*np.sqrt(2*math.pi)))
 
 def create_n_modal_lognormal_function(n):
+    """produce n-modal lognormal function as linear combination of multiple lognormal_functions as defined above with n
+    being the number of contained single functions"""
     # Create the function signature
     args = ['x'] + [f'mu{i + 1}, sigma{i + 1}, A{i + 1}' for i in range(n)]
     func_signature = ', '.join(args)
@@ -20,15 +24,15 @@ def create_n_modal_lognormal_function(n):
     exec(func_def, globals()) # n_modal_lognormal = exec(func_def, globals())
     return n_modal_lognormal
 
-def create_bounds(modalität):
+def create_bounds(modality):
 	bounds_1 = []
 	bounds_2 = []
 	bounds=(bounds_1,bounds_2)
-	for i in range(1, modalität+1):
+	for i in range(1, modality+1):
 		bounds_1.append(0.1)
 		bounds_1.append(1.0)
 		bounds_1.append(0.1)
-	for k in range(1, modalität+1):
+	for k in range(1, modality+1):
 		bounds_2.append(8500)
 		bounds_2.append(np.inf)
 		bounds_2.append(np.inf)
@@ -41,22 +45,23 @@ def save_parma_data(df, origin, fileaddition):
     return
 
 #set of running parameters
-initial_gess = (285.82, 2.6, 299299.88, 211.08, 1.38, 28371.53, 23.392, 1.39, 46609.89, 45.99, 1.34, 25491.47)
+initial_guess = (285.82, 2.6, 299299.88, 211.08, 1.38, 28371.53, 23.392, 1.39, 46609.89, 45.99, 1.34, 25491.47)
 
 #feed data to workspace by calling: param, cov, df = full_function(4, mean_Dry_AS, initial_gess)
-def full_function(n, data, scan_nr, initial_gess= initial_gess):
+def full_function(n, data, scan_nr, initial_guess= initial_guess, used_C="Cn"):
+    X, dX, C = Sup.extract_from_dict(data, used_C)
     b = create_bounds(n)
     fit_nr = scan_nr - 1
     function_type = create_n_modal_lognormal_function(n)
-    params, cov = optimize.curve_fit(function_type, data["mean_X"][0], data["mean_C"][fit_nr],
-                            p0=initial_gess,
+    params, cov = optimize.curve_fit(function_type, X[fit_nr], C[fit_nr],
+                            p0=initial_guess,
                             bounds=(b),
-                            method="trf")
+                            method="trf")  # trf -> bounds are provided
     sigma = np.sqrt(np.diag(cov))
-    plt.plot(data["mean_X"][0], function_type(data["mean_X"][0], *params),
+    plt.plot(X[fit_nr], function_type(X[fit_nr], *params),
              color='red', lw=3, label='multimodal fit')
     for k in range(0,n):
-        plt.plot(data["mean_X"][0], lognormal_function(data["mean_X"][0], *params[k*3:(k+1)*3]),
+        plt.plot(X[fit_nr], lognormal_function(X[fit_nr], *params[k*3:(k+1)*3]),
              color='yellow', lw=1.5, ls=":", label=f"distribution {k+1}")
     plt.legend()
     plt.xscale("log")
