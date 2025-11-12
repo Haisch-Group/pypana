91b20b1695709b61a152324b5998146c99ddd147
KevinRMaier <kevin.r.maier@tum.de>
2025-11-12 10:32:31 +0100

    Checked for typos and functionality and changed np.zeros_like + np.nan to np.full(shape, np.nan)

--- TSI_EM3068_fileread.py
+++ TSI_EM3068_fileread.py
@@ -3,8 +3,7 @@
 TSI_EM3068_fileread.py
 
 Script for Data Evaluation of the TSI EM 3068 Data acquired via NI-Card and Self-Written Labview Script
-The electrometer has a noise current of 1x10^15 ampere according to the manual (and i think there should be a - in the
-power)
+The electrometer has a noise current of 1x10^15 ampere according to the manual
 The output voltage is in a range of 0-10 volts, which correspond to a current of 0-10 picoampere
 Created 2024-08-29
 @written by Kevin Maier (kevin.r.maier@tum.de)
@@ -38,7 +37,7 @@ def import_data(filename):
     # second column is the elapsed time since the start of the program in s
     # third column is the measured electrometer voltage in V
 
-    flowrate = input("Please enter the used flow rate as float in L/min")
+    flowrate = input("Please enter the used flow rate as float in L/min") # could be logged
     flowrate_ccs = float(flowrate)*1000/60
     # manual_start = input("Please enter the start time of the measurement in format mm/dd/yyyy hh:mm:ss AM")
     # start = datetime.strptime(manual_start, '%m/%d/%Y %I:%M:%S %p')
@@ -59,8 +58,8 @@ def import_data(filename):
     # Filling sub_em_voltage and sub_el_time with values every time a new entry that is not zero is found in the sample
     # number column. When the entry changes to a new sample number, the sub... lists are saved to the em_voltage and
     # tot_el_time lists. Also, a counter runs with it to fill the scan_nr list every time the sub... lists are copied
-    # flow_list just runs withit filling the same value consantly, as the flow is not logged currently, but that could
-    # be changed in the future
+    # flow_list just runs with it filling the same value constantly, as the flow is not logged currently
+
     for k in range(1, len(data)):  # runs through all the data line by line
         if data[k, 0] == 0:  # when the first column value in the line is 0, no sample was saved, so it is skipped
             continue
@@ -97,12 +96,9 @@ def import_data(filename):
     max_length = max(map(len, tot_el_time))  # get length of longest measurement for preallocation of arrays
 
     # Preallocation of all arrays and lists that should be exported
-    el_time = np.zeros((nr_scans, max_length))
-    el_time[:] = np.nan
-    Cn = np.zeros((nr_scans, max_length))
-    Cn[:] = np.nan
-    flow_lpm = np.zeros((nr_scans, max_length))
-    flow_lpm[:] = np.nan
+    el_time = np.full((nr_scans, max_length), np.nan)
+    Cn = np.full((nr_scans, max_length), np.nan)
+    flow_lpm = np.full((nr_scans, max_length), np.nan)
     start_time = []
 
     for k in range(nr_scans):
@@ -115,8 +111,7 @@ def import_data(filename):
 
     add_info = pd.DataFrame({"Scan Nr": scan_nr, "Time": start_time, "Comment": comment_list,
                              "Aerosol Flow (L/min)": list(flow_lpm)})
-    # passed flow_lpm in add_info as list, when more actions should be done with flow rate, I would move it out of
-    # add_info and make it a dictionary entry on its own
+    # passed flow_lpm in add_info as list, when more actions should be done with flow rate, maybe move to own dict entry
 
     return Cn, el_time, add_info
 
