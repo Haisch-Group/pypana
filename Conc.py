39ed6852bf095d274d77c0633576da5205090334
KevinRMaier <kevin.r.maier@tum.de>
2025-11-13 14:38:43 +0100

    added used_C and also changed, where "origin" is saved in the merged data file -> now in "add_info" in its own column right at the beginning of the file

--- Conc.py
+++ Conc.py
@@ -25,26 +25,29 @@ import Sup
 # import mpldatacursor  # not used atm
 
 
-def select_data(data, scan_nrs):
+def select_data(data, scan_nrs, used_C="Cn"):
     """select specific CPC msmts from the imported raw data, scan_nrs defines, which measurements to take
     in normal non-pythonian logic (starting count at 1)"""
     py_nrs = Sup.py_logic_converter(scan_nrs)
-    sel_C = np.full((len(py_nrs), data["Cn"].shape[1]), np.nan)
+    sel_C = np.full((len(py_nrs), data[used_C].shape[1]), np.nan)
     # preallocate the np array in the correct size (nr of measurements, nr of measuring data)
     sel_el_time = np.full_like(sel_C, np.nan)
+
     for k in np.arange(len(py_nrs)):  # fill the arrays with the selected data
-        sel_C[k, :] = data["Cn"][py_nrs[k], :]
+        sel_C[k, :] = data[used_C][py_nrs[k], :]
         sel_el_time[k, :] = data["el_time"][py_nrs[k], :]
+
     sel_add_info = pd.DataFrame(columns=list(data["add_info"].columns.values))  # just copy column headers to new DF
-    sel_add_info = pd.concat([sel_add_info, data["add_info"].iloc[py_nrs]])  # fill the new dataframe
+    sel_add_info = pd.concat([sel_add_info, data["add_info"].iloc[py_nrs]], ignore_index=True)  # fill the new dataframe
     sel_results = pd.DataFrame(columns=list(data["results"].columns.values))  # with values from selected data lines
-    sel_results = pd.concat([sel_results, data["results"].iloc[py_nrs]])  # dropping NaN columns would need .dropna("all")
-    sel_data = {"Cn": sel_C, "el_time": sel_el_time, "add_info": sel_add_info, "results": sel_results,
-                "filename": data["filename"], "used_device": data["used_device"]}
+    sel_results = pd.concat([sel_results, data["results"].iloc[py_nrs]], ignore_index=True)  # dropping NaN columns would need .dropna("all")
+
+    sel_data = {used_C: sel_C, "el_time": sel_el_time, "filename": data["filename"], "used_device": data["used_device"],
+                "add_info": sel_add_info, "results": sel_results}
     return sel_data
 
 
-def merge_data(sel_data_list):
+def merge_data(sel_data_list, used_C="Cn"):
     """merges dictionaries of data, should best be used with selected data dicts"""
     merged_data_C = []  # create lists to fill with list of 1D arrays
     merged_data_el_time = []  # done so complicated as arrays can have different length
@@ -56,37 +59,41 @@ def merge_data(sel_data_list):
     merged_results = pd.DataFrame(columns=list(sel_data_list[0]["results"].columns.values))  # and results dfs
 
     for data in sel_data_list:  # append all imported lines to the lists
-        for k in range(len(data["Cn"])):
-            merged_data_C.append(data["Cn"][k])
+
+        for k in range(len(data["el_time"])):
+            merged_data_C.append(data[used_C][k])
             merged_data_el_time.append(data["el_time"][k])
-            n_scans += 1
             time_len_list.append(len(data["el_time"][k]))
-        merged_add_info = pd.concat([merged_add_info, data["add_info"]]) # also concatenate add_info and results of
-        merged_results = pd.concat([merged_results, data["results"]]) # all merged datasets
-        origin.append(data["filename"]) # note down filename for each imported dataset
+            origin.append(data["filename"])  # note down filename for each imported dataset
+            n_scans += 1
+
+        merged_add_info = pd.concat([merged_add_info, data["add_info"]],
+                                    ignore_index=True) # also concatenate add_info and results of
+        merged_results = pd.concat([merged_results, data["results"]], ignore_index=True) # all merged datasets
 
     x_len = max(time_len_list)  # get maximum length of the x-axis elements -> longest x-axis is base for array
     merged_array_C = np.full((n_scans, x_len), np.nan)  # preallocate data arrays
     merged_array_el_time = np.full((n_scans, x_len), np.nan)
+
     for k in range((n_scans)):  # fill arrays row wise with data from list elements
         merged_array_C[k, 0:len(merged_data_C[k])] = merged_data_C[k]
         merged_array_el_time[k, 0:len(merged_data_el_time[k])] = merged_data_el_time[k]
 
     merged_data = {} # prepare merged data dictionary
 
-    merged_data["Cn"] = merged_array_C # fill merged data dictionary
+    merged_data[used_C] = merged_array_C # fill merged data dictionary
     merged_data["el_time"] = merged_array_el_time
-    merged_data["add_info"] = merged_add_info
-    merged_data["results"] = merged_results
     merged_data["used_device"] = sel_data_list[0]["used_device"] # use info from first data set
     merged_data["filename"] = input("Please enter a Path this data should be associated with. - "
                                         "Used for naming figures")
-    merged_data["origin"] = origin
+    merged_add_info.insert(loc=1, column="Origin", value=origin)
+    merged_data["add_info"] = merged_add_info
+    merged_data["results"] = merged_results
 
     return merged_data
 
 
-def select_multiple_data(list_of_tuples):
+def select_multiple_data(list_of_tuples, used_C="Cn"):
     """select specific scans from the imported raw data to then process them, scan_nrs defines, which scans to take
     in normal non-pythonian logic (starting count at 1)
     can only easily select data from one day for comparison
@@ -94,7 +101,7 @@ def select_multiple_data(list_of_tuples):
     sel_data_list = []
     for tuple in list_of_tuples:
         sel_data_list.append(select_data(tuple[0], tuple[1]))
-    sel_merged_data = merge_data(sel_data_list)
+    sel_merged_data = merge_data(sel_data_list, used_C=used_C)
 
     return sel_merged_data
 
@@ -238,22 +245,22 @@ def plot_mean_timeline(data, start_time, end_time, used_C="mean_Cn", colors=Def.
     return ax
 
 
-def plot_calc_conc_n(data, scan_nrs, used_C="calc_conc_n", colors=Def.tum_cm, a=1, save_plot="off"):
+def plot_calc_conc_n(data, scan_nrs, used_C="calc_Cn", colors=Def.tum_cm, a=1, save_plot="off"):
     """ function by Nico: this is a function for distribution derived concentrations"""
 
     py_nrs = Sup.py_logic_converter(scan_nrs)
-    calc_conc_n = data["results"][used_C]
+    calc_C = data["results"][used_C]
     fig, ax = plt.subplots()
 
     ct=0
     if len(py_nrs) == 1:
         k = py_nrs[0]
-        ax.scatter(scan_nrs[0], calc_conc_n[k], edgecolor="black", color=colors[0], alpha=a)
-        # print(f"scan {k} conc. = " + "{:e}".format(float(calc_conc_n[k])) + " P/cm" + u"\u00B3")
+        ax.scatter(scan_nrs[0], calc_C[k], edgecolor="black", color=colors[0], alpha=a)
+        # print(f"scan {k} conc. = " + "{:e}".format(float(calc_C[k])) + " P/cm" + u"\u00B3")
     else:
         for k in range(len(py_nrs)):
-            ax.scatter(scan_nrs[k], calc_conc_n[py_nrs[k]], edgecolor="black", color=colors[0], alpha=a)
-            # print(f"scan {k} conc. = " + "{:e}".format(float(calc_conc_n[k])) + " P/cm" + u"\u00B3")
+            ax.scatter(scan_nrs[k], calc_C[py_nrs[k]], edgecolor="black", color=colors[0], alpha=a)
+            # print(f"scan {k} conc. = " + "{:e}".format(float(calc_C[k])) + " P/cm" + u"\u00B3")
             ct += 1
 
     format_plot(fig, ax)
