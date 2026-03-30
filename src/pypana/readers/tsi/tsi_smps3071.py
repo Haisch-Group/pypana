735849fb619a6a834aa0caf085b13acc10884cf7
Maximilian Böhmichen <34962223+MaximilianBoehmichen@users.noreply.github.com>
2026-03-30 19:58:57 +0200

    add TSI SMPS 3071 can read

--- src/pypana/readers/tsi/tsi_smps3071.py
+++ src/pypana/readers/tsi/tsi_smps3071.py
@@ -5,3 +5,77 @@ This module provides the corresponding reader for the produced files of a TSI SM
 References:
     https://tsi.com/discontinued-products/electrostatic-classifier-3071a
 """
+
+from pathlib import Path
+
+from pypana.readers.base_instrument_reader import BaseInstrumentReader
+from pypana.readers.exceptions.read_error import ReadError
+
+
+class TSISMPS3071InstrumentReader(BaseInstrumentReader):
+    """Instrument reader for TSI SMPS 3071."""
+
+    _device_name = "TSI SMPS 3071"
+
+    @classmethod
+    def can_read(cls, path: Path) -> bool:
+        """Checks whether a given path may include TSI SMPS 3071 output files that can be read.
+
+        Current checks include:
+            - whether the path is a file
+            - Units and Weight are given
+            - the scan lines start with continuous integers
+
+        Args:
+            path: The path to the input file.
+
+        Returns:
+            Whether the read test succeeded when applying the TSI SMPS 3071 format.
+
+        Raises:
+            ReadError: If confident enough that the input is from TSI SMPS 3071, but the data suggests otherwise.
+                This may happen because the input files were manually edited in unsafe places or this package
+                does not yet fully implement this device's formats.
+                Note: the absence of ReadError in this method does not guarantee the input is parseable.
+        """
+        anchors = {"Classifier Model\t3071", "Units", "Weight", "Sample #"}
+        found_anchors = set()
+        data_started = False
+        last_index = 0
+
+        if not path.is_file():
+            return False
+
+        with Path.open(path, "r", encoding="iso-8859-1") as f:
+            for _, line in enumerate(f, start=1):
+                cleaned_line = line.strip()
+
+                if not cleaned_line:
+                    continue
+
+                for anchor in anchors:
+                    if cleaned_line.startswith(anchor):
+                        found_anchors.add(anchor)
+
+                if cleaned_line.startswith("Sample #") and not data_started:
+                    data_started = True
+                    continue
+
+                if not data_started:
+                    continue
+
+                first_column = cleaned_line.split("\t")[0]
+
+                if first_column.isdigit():
+                    current_index = int(first_column)
+
+                    if current_index == last_index + 1:
+                        last_index += 1
+                        continue
+
+                raise ReadError(
+                    message=f"Scan {last_index + 1} appears to be missing",
+                    path=path,
+                )
+
+        return anchors.issubset(found_anchors)
--- tests/readers/defs.py
+++ tests/readers/defs.py
@@ -1,13 +1,16 @@
 from pathlib import Path
 
 from pypana.readers.tsi.tsi_las3340a import TSILAS3340AInstrumentReader
+from pypana.readers.tsi.tsi_smps3071 import TSISMPS3071InstrumentReader
 
 EXAMPLE_FILES_DIR = Path(__file__).resolve().parents[2] / "ExampleFiles"
 
 NON_EXISTING_FILE = EXAMPLE_FILES_DIR / "non_existing_file"
 NON_INSTRUMENT_FILE = Path(__file__).resolve().parents[2] / "pyproject.toml"
 TSILAS3340A_FILE = EXAMPLE_FILES_DIR / "20240704_TSI_LAS3340A"
+TSISMPS3071_FILE = EXAMPLE_FILES_DIR / "20211111_TSI_SMPS3071.txt"
 
 INSTRUMENT_READER_TEST_CASES = [
     (TSILAS3340A_FILE, TSILAS3340AInstrumentReader),
+    (TSISMPS3071_FILE, TSISMPS3071InstrumentReader),
 ]
